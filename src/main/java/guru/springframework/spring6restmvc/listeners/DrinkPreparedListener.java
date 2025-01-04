a48fb1f43326f53410d4f8758da6608ba7178793
John Thompson <john@springframework.guru>
2025-01-04 10:16:33 -0500

    process drink prepared event

---
+++ src/main/java/guru/springframework/spring6restmvc/listeners/DrinkPreparedListener.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring6restmvc.listeners;
+
+import guru.springframework.spring6restmvc.config.KafkaConfig;
+import guru.springframework.spring6restmvc.repositories.BeerOrderLineRepository;
+import guru.springframework.spring6restmvcapi.events.DrinkPreparedEvent;
+import guru.springframework.spring6restmvcapi.model.BeerOrderLineStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DrinkPreparedListener {
+
+    private final BeerOrderLineRepository beerOrderLineRepository;
+
+    @KafkaListener(groupId = "DrinkPreparedListener", topics = KafkaConfig.DRINK_PREPARED_TOPIC)
+    public void listen(DrinkPreparedEvent event) {
+
+        log.debug("Drink Prepared Event Received");
+
+        beerOrderLineRepository.findById(event.getBeerOrderLine().getId()).ifPresentOrElse(beerOrderLine -> {
+
+            beerOrderLine.setOrderLineStatus(BeerOrderLineStatus.COMPLETE);
+
+            beerOrderLineRepository.save(beerOrderLine);
+        }, () -> log.error("Beer Order Line Not Found!"));
+    }
+}
