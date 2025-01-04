a39dc0a02554389517062144f940af38e3128db3
John Thompson <john@springframework.guru>
2025-01-04 10:20:29 -0500

    process drink prepared event

--- src/main/java/guru/springframework/spring6restmvc/config/KafkaConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/KafkaConfig.java
@@ -8,4 +8,5 @@ public class KafkaConfig {
     public static final String DRINK_REQUEST_ICE_COLD_TOPIC = "drink.request.icecold";
     public static final String DRINK_REQUEST_COLD_TOPIC = "drink.request.cold";
     public static final String DRINK_REQUEST_COOL_TOPIC = "drink.request.cool";
+    public static final String DRINK_PREPARED_TOPIC = "drink.prepared";
 }
